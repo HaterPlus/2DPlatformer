e160416c925dde881b66ff541e8f665359dcdfd2
HaterPlus <48578087+HaterPlus@users.noreply.github.com>
2019-03-15 11:17:06 +0700

    Initial commit

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "Exit")
+        {
+
+            Application.Quit();
+
+        }
+
+        if (gameObject.name == "Restart")
+        {
+            
+
+            SceneManager.LoadScene("Level");
+
+            
+        }
+
+        if (gameObject.name == "Play")
+        {
+            
+            SceneManager.LoadScene("Level");
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Unit : MonoBehaviour
+{
+    public virtual void ReceiveDamage()
+    {
+        Die();
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+       
+    }
+}
